95843c45891bd5f5e1493b10808d73726336d363
Anugamannn <158793709+Anugamannn@users.noreply.github.com>
2025-01-31 00:46:45 -0500

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the February 14th my Love María Bautista (fermarrii). I love you so much!";
+  question.innerHTML = "Yay, see you on the February 14th my Love Dikshya. I love you so much!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
